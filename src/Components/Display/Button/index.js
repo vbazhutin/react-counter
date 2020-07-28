5d0dadfdd9254b40f99ce174e5c99e9cc4b28a86
Vlad <vladimir.bazhutin@gmail.com>
2020-07-28 16:01:50 -0500

    dynamic button increment

--- src/Components/Display/Button/index.js
+++ src/Components/Display/Button/index.js
@@ -2,15 +2,23 @@ import PropTypes from 'prop-types'
 import React from 'react';
 import './Button.css'
 
-export const Button = (props) => {
-  const handleClick = () => {
-    console.log('button handler')
-    props.buttonHandler()
+export const Button = ({buttonHandler, increment, buttonTxt}) => {
+  const handleClick = (event) => {
+    buttonHandler(event)
   }
 
-  return <button onClick={handleClick}>{props.buttonTxt}</button>
+  return <button onClick={handleClick} data-increment={increment}>{buttonTxt}</button>
 }
 
-Button.propTypes = { buttonHandler: PropTypes.func, buttonTxt: PropTypes.string.isRequired}
+Button.propTypes = {
+  buttonHandler: PropTypes.func,
+  buttonTxt: PropTypes.string.isRequired,
+  increment: PropTypes.number
+}
+
+Button.defaultProps = {
+  increment: 1,
+  buttonTxt: 'Add 2'
+}
 
 
--- src/Components/Display/index.js
+++ src/Components/Display/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './Display.css'
 import { Button } from './Button'
+//import { Input } from './Input'
 
 export class Display extends React.Component {
   state = {
     currentCount: 0
   }
 
-  updateCount= () => {
-    this.setState( prevState => ({currentCount: prevState.currentCount + 1}))
+  updateCount = ({target}) => {
+    console.log(target)
+    this.setState( prevState => ({currentCount: prevState.currentCount + Number(target.dataset.increment)}))
   }
+
 
   render() {
 
     return (
       <div>
         <p>{this.state.currentCount}</p>
-        <Button buttonHandler={this.updateCount} buttonTxt='Add 1' />
-        <Button buttonHandler={this.updateCount} buttonTxt='Add 2' />
+        {/* <input >Type In a # Counter</input> */}
+        <Button buttonHandler={this.updateCount} buttonTxt='Add 1' increment = {1}/>
+        <Button buttonHandler={this.updateCount} buttonTxt='Add 2' increment = {2000}/>
       </div>
     )
   }
